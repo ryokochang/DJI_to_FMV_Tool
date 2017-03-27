b4455169f5da8d5e773362a75cc79209baefb67e
Alex Chang <ryokochang@hotmail.com>
2017-03-27 08:56:22 -0300

    First Version

---
+++ DJI_to_FMV/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DJI_to_FMV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_load = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox_save = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox_FOV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_load
+            // 
+            this.textBox_load.Location = new System.Drawing.Point(26, 24);
+            this.textBox_load.Name = "textBox_load";
+            this.textBox_load.Size = new System.Drawing.Size(433, 20);
+            this.textBox_load.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox_save
+            // 
+            this.textBox_save.Location = new System.Drawing.Point(26, 70);
+            this.textBox_save.Name = "textBox_save";
+            this.textBox_save.Size = new System.Drawing.Size(433, 20);
+            this.textBox_save.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(245, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "GO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox_FOV
+            // 
+            this.textBox_FOV.Location = new System.Drawing.Point(107, 117);
+            this.textBox_FOV.MaxLength = 4;
+            this.textBox_FOV.Name = "textBox_FOV";
+            this.textBox_FOV.Size = new System.Drawing.Size(29, 20);
+            this.textBox_FOV.TabIndex = 5;
+            this.textBox_FOV.Text = "0";
+            this.textBox_FOV.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "HorizontalFOV:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 161);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_FOV);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_save);
+            this.Controls.Add(this.textBox_load);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DJI to FMV Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_load;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox_save;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox_FOV;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DJI_to_FMV/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace DJI_to_FMV
+{
+  public partial class Form1 : Form
+    {
+        string csv_load,csv_save;
+        int horizontalFOV;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            diag.Filter = "Csv Files|*.Csv";
+            diag.ShowDialog();
+            if (diag.CheckFileExists)
+            {
+                textBox_load.Text = diag.FileName;
+                this.csv_load = diag.FileName;             
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saved = new SaveFileDialog();
+            saved.Filter = "Csv Files|*.Csv";
+            saved.DefaultExt = ".csv";
+            saved.ShowDialog();
+            if (!saved.CheckFileExists)
+            {
+                textBox_save.Text = saved.FileName;
+                this.csv_save = saved.FileName;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fileRead = File.OpenRead(csv_load);
+            var fileSave = File.OpenWrite(csv_save);
+            var reader = new StreamReader(fileRead);
+            var writer = new StreamWriter(fileSave);
+            string newLine;
+            string timestamp = "";
+            string longFormats = "yyyy/MM/dd HH:mm:ss.fff";
+            DateTime myDateL,Ldate;
+            DateTime myDateS,Sdate;
+            List<string> list1 = new List<string>();
+            int once = 1;
+            while (!reader.EndOfStream)
+            {        
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                if (values[2] == "Recording")
+                {
+                    if (once == 2)
+                    {
+                        if (DateTime.TryParseExact(values[0],"yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture,DateTimeStyles.None , out Sdate))
+                        {
+                            double unixDate = DateTimeToUnixTimestamp(Sdate) * 100000;
+                            timestamp = unixDate.ToString().Replace(".", "") + "0000";
+                        }
+                        else
+                        {
+                            myDateL = DateTime.ParseExact(values[0], longFormats, CultureInfo.InvariantCulture);
+                            double unixDate = DateTimeToUnixTimestamp(myDateL) * 100000;
+                            timestamp = unixDate.ToString().Replace(".","")+"0";
+                        }
+                       
+                    }
+                    newLine = String.Format(
+                    "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}",
+                    timestamp, values[3], values[9], values[10], values[12], values[16],
+                    values[17], values[18], values[65], values[66], values[67], textBox_FOV.Text);
+                    if (once == 1)
+                    {
+                        newLine = String.Format(
+                        "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}",
+                        "TimeStamp", "PlatformTrueAirSpeed", "SensorLatitude", "SensorLongitude", "SensorAltitude", "PlatformPitch",
+                        "PlatformRoll", "PlatformHeading", "SensorRelativeElevation",
+                        "SensorRelativeRoll", "SensorRelativeAzimuth", "HorizontalFOV");
+                    }
+
+                    once = 2;
+                    writer.WriteLine(newLine);
+                }
+            }
+
+            writer.Close();
+            reader.Close();
+            fileRead.Close(); // ter que dar close no arquivo
+            fileSave.Close(); // ter que dar close no arquivo
+        }
+        public static double DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (TimeZoneInfo.ConvertTimeToUtc(dateTime) -
+                   new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+        }
+    }
+}
+
